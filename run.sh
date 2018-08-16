d46cd9a42f3ab14074abfe92d14225c5b5b6b3ea
j3br <jens.bruhn@outlook.com>
2018-08-16 22:30:25 +0200

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+cat > /var/www/html/application/config/config.php <<EOF
+<?php
+\$config['language'] = 'en_US';
+\$config['show_inactive_clients'] = false;
+\$config['hide_empty_pools'] = true;
+\$config[0]['label'] = 'Database';
+\$config[0]['host'] = '$DB_HOST';
+\$config[0]['login'] = '$DB_USER';
+\$config[0]['password'] = '$DB_PASS';
+\$config[0]['db_name'] = '$DB_NAME';
+\$config[0]['db_type'] = '$DB_TYPE';
+\$config[0]['db_port'] = '$DB_PORT';
+?>
+EOF
+
+echo "date.timezone = $PHP_TIMEZONE" >> /etc/php.ini
+
+/usr/sbin/httpd -D FOREGROUND
